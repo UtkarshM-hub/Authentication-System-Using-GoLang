66a974c631a7e34b329e2895190b16e33fcbbe69
UtkarshM-Hub <utmandape4@gmail.com>
2022-10-17 15:47:39 +0530

    chore: minor change

--- GoAuth/Login.go
+++ GoAuth/Login.go
@@ -12,7 +12,7 @@ func Login[T map[string] any](mongoClient mongo.Client,collectionName string,dat
 		return errors.New(fmt.Sprint(collectionName,"is not a valid collection name"))
 	}
 	if _,exist:=data[key];!exist{
-		return errors.New(fmt.Sprint(key,"is not a valid key"))
+		return errors.New(fmt.Sprint(key," is not a valid key"))
 	} else{
 		fmt.Println(data[key]);
 	}
