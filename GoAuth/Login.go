a9d945e85b284df76a03be4b1886cc8da8cee270
UtkarshM-Hub <utmandape4@gmail.com>
2022-10-18 16:02:56 +0530

    Feat: test database integration

--- GoAuth/Login.go
+++ GoAuth/Login.go
@@ -16,8 +16,11 @@ func Login(mongoClient mongo.Client,DatabaseName string,collectionName string,da
 	if _,exist:=data[key];!exist{
 		return errors.New(fmt.Sprint(key," is not a valid key"));
 	} 
+	result:=struct{
+		name string
+	}{}
 	user:=bson.D{{key,data[key]}}
-	result:=mongoClient.Database(DatabaseName).Collection(collectionName).FindOne(context.TODO(),user)
+	mongoClient.Database(DatabaseName).Collection(collectionName).FindOne(context.TODO(),user).Decode(&result)
 	fmt.Println(result)
 	return nil
 }
