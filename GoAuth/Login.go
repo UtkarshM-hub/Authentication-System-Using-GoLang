183b042d40e749f6a74ef1f860bf346d10ea74cd
UtkarshM-Hub <utmandape4@gmail.com>
2022-10-18 15:22:21 +0530

    Feat: check database integration

--- GoAuth/Login.go
+++ GoAuth/Login.go
@@ -1,20 +1,23 @@
 package GoAuth
 
 import (
+	"context"
 	"errors"
 	"fmt"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func Login[T map[string] any](mongoClient mongo.Client,collectionName string,data T,key string) (error) {
+func Login(mongoClient mongo.Client,DatabaseName string,collectionName string,data map[string] any,key string) (error) {
 	if collectionName==""{
 		return errors.New(fmt.Sprint(collectionName,"is not a valid collection name"));
 	}
 	if _,exist:=data[key];!exist{
 		return errors.New(fmt.Sprint(key," is not a valid key"));
-	} else{
-		fmt.Println(data[key]);
-	}
+	} 
+	user:=bson.D{{key,data[key]}}
+	result:=mongoClient.Database(DatabaseName).Collection(collectionName).FindOne(context.TODO(),user)
+	fmt.Println(result)
 	return nil
 }
