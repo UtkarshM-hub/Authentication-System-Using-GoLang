71e8c07f4d9d7d68e3995f0023fd3d7db08c105d
UtkarshM-Hub <utmandape4@gmail.com>
2022-10-19 16:33:26 +0530

    Feat: test mongodb integration

--- GoAuth/Login.go
+++ GoAuth/Login.go
@@ -17,12 +17,12 @@ func Login(mongoClient mongo.Client,DatabaseName string,collectionName string,da
 	if _,exist:=data[key];!exist{
 		return errors.New(fmt.Sprint(key," is not a valid key"));
 	} 
-	var result=struct{
-		name string
-	}{}
+	// var result=struct{
+	// 	name string
+	// }{}
 	// user:=bson.D{primitive.E{Key:key,Value:data[key]}}
-	mongoClient.Database("GoAuth").Collection("user").FindOne(context.TODO(),bson.D{primitive.E{Key: "name",Value: "UtkarshM-hub"}}).Decode(&result)
+	mongoClient.Database("GoAuth").Collection("user").FindOne(context.TODO(),bson.D{primitive.E{Key: "name",Value: "UtkarshM-hub"}})
 	// mongoClient.Database(DatabaseName).Collection(collectionName).FindOne(context.TODO(),user).Decode(&result)
-	fmt.Println(result)
+	fmt.Println("this is the result")
 	return nil
 }
