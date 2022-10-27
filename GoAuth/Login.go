abbcb0ce302a5e811aa2bc4463d1f9d24e544027
UtkarshM-Hub <utmandape4@gmail.com>
2022-10-27 16:23:33 +0530

    Feat: Add Password Hashing
    Added password hashing using bcrypt package

--- GoAuth/Login.go
+++ GoAuth/Login.go
@@ -8,10 +8,11 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func Login(mongoClient mongo.Client,DatabaseName string,collectionName string,data map[string] any,key string) (error) {
-
+func Login(mongoClient mongo.Client,DatabaseName string,collectionName string,data map[string]any,key string,AuthKey string) (error) {
+	
 	// check if the collection name is valid or not
 	if collectionName==""{
 		return errors.New(fmt.Sprint(collectionName,"is not a valid collection name"));
@@ -20,6 +21,9 @@ func Login(mongoClient mongo.Client,DatabaseName string,collectionName string,da
 	// check if the given property is valid or not
 	if _,exist:=data[key];!exist{
 		return errors.New(fmt.Sprint(key," is not a valid key"));
+	} 
+	if _,exist:=data[AuthKey];!exist{
+		return errors.New(fmt.Sprint(key," is not a valid Authentication key name"));
 	} 
 
 	var result bson.M
@@ -30,11 +34,25 @@ func Login(mongoClient mongo.Client,DatabaseName string,collectionName string,da
 	// find the user in database
 	if err:=mongoClient.Database(DatabaseName).Collection(collectionName).FindOne(context.TODO(),user).Decode(&result); err!=nil{
 		// return error if there is no data in database
-		return errors.New("user does not exist");
+		return errors.New("user does not exist")
 	}
 
+	// Convert any to string
+	inputString:=data[AuthKey].(string)
+	passString:=result[AuthKey].(string)
+	
+	// Convert string to []byte
+	bytePass:=[]byte(inputString)
+	byteDataPass:=[]byte(passString)
+	
+	// Compare the passwords 
+	err:=bcrypt.CompareHashAndPassword(byteDataPass,bytePass)
+	if(err!=nil){
+		return errors.New("invalid Password")
+	}
+	
 	// return error if the user does not exist
-	if(len(result)==0){
+	if len(result)==0 {
 		return errors.New("user does not exist")
 	}
 	return nil
