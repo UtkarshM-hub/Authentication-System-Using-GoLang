d39e554fd50551db1af75194952b237deb44e653
UtkarshM-Hub <utmandape4@gmail.com>
2022-10-28 16:22:51 +0530

    Fix: Make use of IfExist in Signup

--- GoAuth/Signup.go
+++ GoAuth/Signup.go
@@ -10,23 +10,20 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func Signup(mongoClient mongo.Client,DatabaseName string,CollectionName string,Data map[string]any,key string) (any,error) {
+func Signup(mongoClient mongo.Client,DatabaseName string,CollectionName string,Data map[string]any,key string) error {
 	// check if the collection name is valid or not
 	if CollectionName==""{
-		return nil,errors.New(fmt.Sprint(CollectionName,"is not a valid collection name"));
+		return errors.New(fmt.Sprint(CollectionName,"is not a valid collection name"));
 	}
  
 	// check if the given property is valid or not
 	if _,exist:=Data[key];!exist{
-		return nil,errors.New(fmt.Sprint(key," is not a valid key"));
+		return errors.New(fmt.Sprint(key," is not a valid key"));
 	}
 
-	var result bson.M
-	query:=bson.D{primitive.E{Key: key,Value: Data[key]}}
-	mongoClient.Database(DatabaseName).Collection(CollectionName).FindOne(context.TODO(),query).Decode(&result)
-
-	if len(result)!=0{
-		return nil,errors.New("user already exist")
+	err:=IfExist(mongoClient,DatabaseName,CollectionName,key,Data[key]);
+	if err!=nil{
+		return err
 	}
 
 	input :=bson.D{}
@@ -34,11 +31,13 @@ func Signup(mongoClient mongo.Client,DatabaseName string,CollectionName string,D
 	for k:=range Data{
 		input = append(input, primitive.E{Key: k,Value: Data[k]})
 	}
-
+	fmt.Println(input)
 	response,err:=mongoClient.Database(DatabaseName).Collection(CollectionName).InsertOne(context.TODO(),input)
 	if err!=nil{
-		return nil,errors.New("error occured while signup")
+		return errors.New("error occured while signup")
 	}
 
-	return response,nil
+	fmt.Println(response)
+
+	return nil
 }
