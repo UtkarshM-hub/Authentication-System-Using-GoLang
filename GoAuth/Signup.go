13081ba3e5d13ac618da48833c472ea8da904089
UtkarshM-Hub <utmandape4@gmail.com>
2022-10-28 16:31:03 +0530

    Fix: Return objectId as a response from Signup

--- GoAuth/Signup.go
+++ GoAuth/Signup.go
@@ -10,34 +10,34 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func Signup(mongoClient mongo.Client,DatabaseName string,CollectionName string,Data map[string]any,key string) error {
+func Signup(mongoClient mongo.Client,DatabaseName string,CollectionName string,Data map[string]any,key string) (any,error) {
 	// check if the collection name is valid or not
 	if CollectionName==""{
-		return errors.New(fmt.Sprint(CollectionName,"is not a valid collection name"));
+		return nil,errors.New(fmt.Sprint(CollectionName,"is not a valid collection name"));
 	}
  
 	// check if the given property is valid or not
 	if _,exist:=Data[key];!exist{
-		return errors.New(fmt.Sprint(key," is not a valid key"));
+		return nil,errors.New(fmt.Sprint(key," is not a valid key"));
 	}
 
+	// Check if the document already exist in database or not
 	err:=IfExist(mongoClient,DatabaseName,CollectionName,key,Data[key]);
 	if err!=nil{
-		return err
+		return nil,err
 	}
 
+	// get input data in bson format
 	input :=bson.D{}
-
 	for k:=range Data{
 		input = append(input, primitive.E{Key: k,Value: Data[k]})
 	}
-	fmt.Println(input)
+
+	// Insert the data
 	response,err:=mongoClient.Database(DatabaseName).Collection(CollectionName).InsertOne(context.TODO(),input)
 	if err!=nil{
-		return errors.New("error occured while signup")
+		return nil,errors.New("error occured while signup")
 	}
 
-	fmt.Println(response)
-
-	return nil
+	return response,nil
 }
