32a5aecd5a5780034b6c9a66f35dd0b8ceafc370
Utkarsh Mandape <70505181+UtkarshM-hub@users.noreply.github.com>
2022-10-28 16:35:17 +0530

    Chore: Added comments to IfExist method

--- GoAuth/IfExist.go
+++ GoAuth/IfExist.go
@@ -11,14 +11,17 @@ import (
 
 func IfExist(mongoClient mongo.Client,DatabaseName string,CollectionName string,key string,value any)(error){
 
+	// Check whether the input key is empty or not
 	if len(key)==0{
 		return errors.New("key or the value provided is invalid")
 	}
-
+	
+	//query to find whether the object already exist in the database or not 	
 	var result bson.M
 	query:=bson.D{primitive.E{Key: key,Value: value}}
 	mongoClient.Database(DatabaseName).Collection(CollectionName).FindOne(context.TODO(),query).Decode(&result)
 
+	// return if there are no objects in database
 	if len(result)!=0{
 		return errors.New("user already exist")
 	}
